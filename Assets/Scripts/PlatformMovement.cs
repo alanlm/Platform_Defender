8dc40e02321d304bcf0b18422618f9b4fc999553
Alan Manalang <a_rabbit93@yahoo.com>
2017-10-14 13:01:31 -0700

    removed debug statements and updated comments

--- Assets/Scripts/PlatformMovement.cs
+++ Assets/Scripts/PlatformMovement.cs
@@ -11,18 +11,16 @@ public class PlatformMovement : MonoBehaviour {
 		float rightEdge = transform.position.x + (transform.childCount / 2) + 0.5f; 
 		float leftEdge = transform.position.x - (transform.childCount / 2) + 0.5f;  
 
-		Debug.Log ("Right Edge: " + rightEdge); 
-		Debug.Log ("Left Edge: " + leftEdge); 
-
 		// calculate boundary of platform 
 		float angle = Camera.main.fieldOfView * Mathf.Deg2Rad; 
 		float distToCamera = Camera.main.transform.position.z; 
 		float boundary = Mathf.Abs(distToCamera) * Mathf.Tan (angle); 
 
-		// if platform edges reach camera's boundary 
+		// if platform edges reach camera's boundaries, reverse direction 
 		if (rightEdge >= Mathf.Floor (boundary) || leftEdge <= Mathf.Floor (-boundary))
 			platformSpeed = -platformSpeed; 
-		
+
+		// move platform 
 		transform.Translate(Time.deltaTime * platformSpeed, 0f, 0f); 
 	}
 }
