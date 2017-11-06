ceae539da26b6c1c59c0ee0eb474744e30cccfbc
Alan Manalang <a_rabbit93@yahoo.com>
2017-11-06 13:20:54 -0800

    player can jump

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float playerSpeed; 
+	public float jumpHeight; 
+
+	bool canJump; 
+
+	void Update ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal"); 
+
+		if (Input.GetButtonDown ("Jump") && canJump) {
+			canJump = false; 
+			GetComponent<Rigidbody> ().velocity = Vector3.up * jumpHeight; 
+		}
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0f, 0f); 
+
+		transform.position += movement / 10 * playerSpeed; 
+	}
+
+	void OnCollisionStay() 
+	{
+		canJump = true; 
+	}
+
+}
