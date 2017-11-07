b5eacab7c97cdc62fd3b9f587a64be67c1ef4015
Alan Manalang <a_rabbit93@yahoo.com>
2017-11-07 15:39:22 -0800

    player's x movements restricted within screen width

---
+++ Assets/Scripts/DisableByContact.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableByContact : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.CompareTag ("Player") || other.CompareTag("Platform"))
+			return; 
+		
+		gameObject.SetActive (false); 
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float playerSpeed; 
+	public float jumpHeight; 
+
+	bool canJump; 
+
+	void Update ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal"); 
+
+		if (Input.GetButtonDown ("Jump") && canJump) {
+			canJump = false; 
+			GetComponent<Rigidbody> ().velocity = Vector3.up * jumpHeight; 
+		}
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0f, 0f); 
+
+		transform.position += movement / 10 * playerSpeed; 
+
+		// restricting player movement to be within screen Camera.main.WorldToScreenPoint(new Vector3(leftEdge, 0f, 0f)).x
+		transform.position = new Vector3(
+			Mathf.Clamp(transform.position.x, -30, 30),
+			transform.position.y, 
+			transform.position.z); 
+	}
+
+	void OnCollisionStay() 
+	{
+		canJump = true; 
+	}
+
+}
